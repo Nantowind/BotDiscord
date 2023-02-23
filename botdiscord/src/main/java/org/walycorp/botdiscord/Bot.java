d4a0540b01ce16946c1e4293838178515d149af9
Nantowind <walterdami1234@gmail.com>
2023-02-23 09:08:35 -0300

    Se agregaron los primeros / de manera test y estan funcionando

--- botdiscord/src/main/java/org/walycorp/botdiscord/Bot.java
+++ botdiscord/src/main/java/org/walycorp/botdiscord/Bot.java
@@ -1,6 +1,8 @@
 package org.walycorp.botdiscord;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.requests.GatewayIntent;
@@ -9,6 +11,7 @@ import net.dv8tion.jda.api.sharding.ShardManager;
 import net.dv8tion.jda.api.utils.ChunkingFilter;
 import net.dv8tion.jda.api.utils.MemberCachePolicy;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
+import org.walycorp.botdiscord.commands.CommandManager;
 import org.walycorp.botdiscord.listeners.EventListener;
 
 import javax.security.auth.login.LoginException;
@@ -33,7 +36,6 @@ public class Bot {
         String TOKEN = config.get("TOKEN");
 
 
-
         DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(TOKEN);
         builder.enableIntents(GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES,
                 GatewayIntent.GUILD_MESSAGE_REACTIONS, GatewayIntent.DIRECT_MESSAGE_REACTIONS,
@@ -48,7 +50,7 @@ public class Bot {
         // Construye el ShardManager a partir del builder
         shardManager = builder.build();
         // registrar listeners
-        shardManager.addEventListener(new EventListener());
+        shardManager.addEventListener(new EventListener(), new CommandManager());
     }
 
 
@@ -71,6 +73,7 @@ public class Bot {
     // Método main que inicia el bot
     public static void main(String[] args) throws LoginException {
         try {
+
             Bot bot = new Bot();
         } catch (LoginException e) {
             // Si se produce una excepción de inicio de sesión, muestra un mensaje de error.
---
+++ botdiscord/src/main/java/org/walycorp/botdiscord/commands/CommandManager.java
@@ -0,0 +1,62 @@
+package org.walycorp.botdiscord.commands;
+
+import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.events.guild.GuildReadyEvent;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.session.ReadyEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class CommandManager extends ListenerAdapter {
+
+    /**
+     * Listens for slash commands and responds accordingly
+     */
+    @Override
+    public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
+        String command = event.getName();
+        if (command.equals("welcome")) {
+            // Run the 'ping' command
+            String userTag = event.getUser().getAsTag();
+            event.reply("Welcome to the server, **" + userTag + "**!").queue();
+        } else if (command.equals("roles")) {
+            // run the 'roles' command
+            event.deferReply().queue();
+            String response = "";
+            for (Role role : event.getGuild().getRoles()) {
+                response += role.getAsMention() + "\n";
+            }
+            event.getHook().sendMessage(response).queue();
+        }
+    }
+
+    /**
+     * Registers slash commands as GUILD commands (max 100).
+     * These commands will update instantly and are great for testing.
+     */
+    @Override
+    public void onGuildReady(@NotNull GuildReadyEvent event) {
+        List<CommandData> commandData = new ArrayList<>();
+        commandData.add(Commands.slash("welcome", "Get welcomed by the bot"));
+        commandData.add(Commands.slash("roles", "Display all roles on the server"));
+        event.getGuild().updateCommands().addCommands(commandData).queue();
+    }
+
+    /**
+     * Registers slash commands as GLOBAL commands (unlimited).
+     * These commands may take up to an hour to update.
+     */
+    @Override
+    public void onReady(@NotNull ReadyEvent event) {
+        List<CommandData> commandData = new ArrayList<>();
+        commandData.add(Commands.slash("welcome", "Get welcomed by the bot"));
+        commandData.add(Commands.slash("roles", "Display all roles on the server"));
+        event.getJDA().updateCommands().addCommands(commandData).queue();
+    }
+}
